56a2fd487c3a2394343808b06f39358551cc0533
xScoopy <JLeary070@gmail.com>
2021-05-05 18:08:08 -0500

    altered server.js to work with directory name change

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 require('./config/db-setup.js')
 
 // Routes
-const router = require('./routes/index.js')
+const router = require('./controllers/index.js')
 app.use(router)
 
 // Start Server
