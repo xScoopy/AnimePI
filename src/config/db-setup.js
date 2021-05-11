ba6792534b09a5db84c1f156290fe070db7e778e
xScoopy <JLeary070@gmail.com>
2021-05-11 18:26:37 -0500

    added db connection string to atlas cluster

--- src/config/db-setup.js
+++ src/config/db-setup.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 // connect to mongo db
-const mongoUri = process.env.MONGODB_URI || 'AnimePI'
+const mongoUri = `mongodb+srv://scoopy:${process.env.MONGODB_PASSWORD}@webcluster.jdw9h.mongodb.net/AnimePI?retryWrites=true&w=majority`|| 'AnimePI'
 mongoose.set('useUnifiedTopology', true)
 mongoose.set('useFindAndModify', false)
 mongoose.connect(mongoUri, { useNewUrlParser: true })
